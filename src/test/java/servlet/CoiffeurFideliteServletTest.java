7e3fa47c645fad87e004a511546650927191c050
Doha IDRISSI AZAMI <dohaazami004@gmail.com>
2026-02-12 13:35:17 +0100

    tests

--- src/test/java/servlet/CoiffeurFideliteServletTest.java
+++ src/test/java/servlet/CoiffeurFideliteServletTest.java
@@ -10,16 +10,16 @@ import model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class AdminFideliteServletTest {
+class CoiffeurFideliteServletTest {
 
-    private AdminFideliteServlet servlet;
+    private CoiffeurFideliteServlet servlet;
     private HttpServletRequest request;
     private HttpServletResponse response;
     private HttpSession session;
 
     @BeforeEach
     void setup() {
-        servlet = new AdminFideliteServlet();
+        servlet = new CoiffeurFideliteServlet();
         request = mock(HttpServletRequest.class);
         response = mock(HttpServletResponse.class);
         session = mock(HttpSession.class);
@@ -34,13 +34,12 @@ class AdminFideliteServletTest {
         servlet.doGet(request, response);
 
         verify(response).sendRedirect(anyString());
-        verifyNoMoreInteractions(response);
     }
 
     @Test
-    void redirectionSiUserNonAdmin() throws Exception {
+    void redirectionSiUserNonCoiffeur() throws Exception {
         User user = new User();
-        user.setRole("coiffeur");
+        user.setRole("admin");
 
         when(session.getAttribute("user")).thenReturn(user);
 
